3d03868456fe804834ff6836b4efcfdf7a64d2c6
dhananjay-0987 <tdhananjay788@gmail.com>
2025-09-10 16:19:33 +0530

    hide the details

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER =  "dhananjaykthakur17@gmail.com"
-EMAIL_HOST_PASSWORD ="pkoj ambt wrpu yjui"
+# EMAIL_HOST_USER =  "enter your email"
+# EMAIL_HOST_PASSWORD ="enter your app password"
